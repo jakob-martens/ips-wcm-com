aa5046f4011855a3f52539b47a458c77dcdeceea
Jakob Martens <jakob.martens@gmail.com>
2022-12-09 01:03:39 +0100

    Added module structure

---
+++ WCMClient/module.php
@@ -0,0 +1,20 @@
+<?
+class WCMClient extends IPSModule {
+
+    public function Create() {
+        parent::Create();
+        
+        $this->RegisterPropertyString("URL", "[]");
+        $this->RegisterPropertyString("Username", "[]");
+        $this->RegisterPropertyString("Password", "[]");
+    }
+    
+    public function ApplyChanges() {
+        parent::ApplyChanges();
+    }
+    
+    public function MeineErsteEigeneFunktion() {
+        // Selbsterstellter Code
+    }
+}
+?>
