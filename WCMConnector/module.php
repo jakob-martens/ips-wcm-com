ac4627964b07b31c3fca08a4e71061a2bc388c64
Jakob Martens <jakob.martens@gmail.com>
2022-12-10 01:53:14 +0100

    Added 'Betriebsart Heizkreis' status update

--- WCMConnector/module.php
+++ WCMConnector/module.php
@@ -3,20 +3,52 @@ require __DIR__ . '/../libs/wcm-client/wcm.php';
 
 class WCMConnector extends IPSModule {
 
+
     public function Create() {
         parent::Create();
         
         $this->RegisterPropertyString("URL", "");
         $this->RegisterPropertyString("Username", "");
         $this->RegisterPropertyString("Password", "");
+        $this->RegisterPropertyInteger("UpdateInterval", 0);
+        $this->RegisterPropertyInteger("FirstHK", 2);
+        $this->RegisterPropertyInteger("LastHK", 5);
+        
+        $this->RegisterTimer("Update", 0, "WCM_UpdateWCMStatus(".$this->InstanceID.");");
+        
+        $this->CreateVarProfileWCMBetriebsartHK();
     }
     
     public function ApplyChanges() {
         parent::ApplyChanges();
+        
+        for($i = $this->ReadPropertyInteger("FirstHK"); $i <= $this->ReadPropertyInteger("LastHK"); $i++) {
+            $this->RegisterVariableInteger("Heizkreis".$i, "Heizkreis ".$i, "WCM.BetriebsartHK");
+        }
+        
+        $this->UpdateWCMStatus();
+        
+        $this->SetTimerInterval("Update", $this->ReadPropertyInteger("UpdateInterval") * 1000);
     }
     
-    public function MeineErsteEigeneFunktion() {
+    public function UpdateWCMStatus() {
         echo $this->ReadPropertyString("URL");
+        $api = new Weishaupt(new WeishauptOptions($this->ReadPropertyString("URL"), $this->ReadPropertyString("Username"), $this->ReadPropertyString("Password")));
+        
+        for($i = $this->ReadPropertyInteger("FirstHK"); $i <= $this->ReadPropertyInteger("LastHK"); $i++) {
+            $params = $api->getHKParameters($i);
+            $this->SetValue("Heizkreis".$i, $params->getIterator()[0]->DATA);
+        }
     }
+    
+    private function CreateVarProfileWCMBetriebsartHK() {
+		if (!IPS_VariableProfileExists("WCM.BetriebsartHK")) {
+			IPS_CreateVariableProfile("WCM.BetriebsartHK", 1);
+			IPS_SetVariableProfileValues("WCM.BetriebsartHK", 1, 255, 0);
+            foreach(BetriebsartHK as $name => $value) {
+                IPS_SetVariableProfileAssociation("WCM.BetriebsartHK", $value, $name, "", -1);
+            }
+		 }
+	}
 }
 ?>
